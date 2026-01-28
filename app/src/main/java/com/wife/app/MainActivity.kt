6f175ca3c54347db7297909f36cd0cd491f172d3
trio92153 <trio92153@gmail.com>
2026-01-28 12:12:42 +0800

    Add files via upload

---
+++ app/src/main/java/com/wife/app/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.wife.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Text(text = "WIFE ❤️")
+            }
+        }
+    }
+}
