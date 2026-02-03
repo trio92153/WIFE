518b51fb509bda8c08f1f294092132f83b8f80ee
trio92153 <trio92153@gmail.com>
2026-02-03 12:05:31 +0800

    Add files via upload

---
+++ app/src/main/java/com/wife/app/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.wife.app
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+import android.view.Gravity
+import android.graphics.Color
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val tv = TextView(this)
+        tv.text = "WIFE ❤️\nAndroid 11 Stable"
+        tv.textSize = 24f
+        tv.setTextColor(Color.WHITE)
+        tv.setBackgroundColor(Color.BLACK)
+        tv.gravity = Gravity.CENTER
+
+        setContentView(tv)
+    }
+}
