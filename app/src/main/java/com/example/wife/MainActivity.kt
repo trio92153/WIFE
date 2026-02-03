ab241b87c21435174e571dcb451f35a5edb6e3d4
trio92153 <trio92153@gmail.com>
2026-02-03 16:18:40 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/wife/MainActivity.kt
+++ app/src/main/java/com/example/wife/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.wife
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startForegroundService(Intent(this, TimerService::class.java))
     }
 }
